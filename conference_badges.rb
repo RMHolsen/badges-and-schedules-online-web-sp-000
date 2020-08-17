f511e7dd06b7d58348647a38c8beb32e2169cc5c
Rosario Holsen <rholsen@gmail.com>
2020-08-17 15:34:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def batch_badge_creator(names)
   badges = []
-  names.each {|name| badges << "Hello, my name is #{name}"}
+  names.each {|name| badges << "Hello, my name is #{name}."}
   badges
 end 
