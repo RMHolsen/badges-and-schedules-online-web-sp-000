ff7b27675a46cbabdd3cd4bdb86054c11d058a8d
Rosario Holsen <rholsen@gmail.com>
2020-08-17 16:37:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,6 @@ def assign_rooms(names)
 end 
   
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  puts batch_badge_creator
+  puts assign_rooms
 end 
